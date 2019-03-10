01353f6045c38b590e61b1266c069b115253dd3f
862532665 <862532665@qq.com>
2019-03-10 21:44:09 +0800

    第一次

---
+++ src/main/java/a/T.java
@@ -0,0 +1,12 @@
+package a;
+
+/**
+ * @author: 11827
+ * @date: 2019/3/10 21:32
+ * @description:
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
