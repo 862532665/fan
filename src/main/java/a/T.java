c109a283169e5ba2eeb67690cadabf4d884302f6
862532665 <862532665@qq.com>
2019-03-10 22:09:03 +0800

    3333wss

--- src/main/java/a/T.java
+++ src/main/java/a/T.java
@@ -7,7 +7,7 @@ package a;
  */
 public class T {
     public static void main(String[] args) {
-        System.out.println(3);
-        System.out.println(4);
+        System.out.println(31);
+        System.out.println(41);
     }
 }
